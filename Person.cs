62165bf033c65ea9bd8427380f51f5bc1b10ea09
paramitasaha17 <mkspsaha17@gmail.com>
2019-10-15 21:18:45 +0600

    text File Changed

---
+++ Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInformationAppWalkthrough2
+{
+    class Person
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private int point;
+
+        public  string FirstName { set; get; }
+        public  string MiddleName { set; get; }
+        public string LastName { set; get; }
+
+        public Person()
+        {
+            point = 10;
+        }
+
+        public Person(string firstName, string lastName):this()
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public Person(string firstName, string middleName, string lastName):this(firstName,lastName)
+        {
+           // this.firstName = firstName;
+            this.MiddleName = middleName;
+           // this.lastName = lastName;
+        }
+
+        public string GetFullName()
+        {
+            return firstName + " " + middleName + " " + lastName;
+        }
+        public string GetReverseName()
+        {
+            return null;
+        }
+    }
+}
---
+++ PersonInformationUi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonInformationAppWalkthrough2
+{
+    public partial class PersonInformationUi : Form
+    {
+         Person aPerson;
+         Person bPerson;
+        public PersonInformationUi()
+        {
+            InitializeComponent();
+        }
+
+
+       
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+           // aPerson.firstName = firstNameTextBox.Text;
+           // aPerson.middleName = middleNameTextBox.Text;
+          //  aPerson.lastName = lastNameTextBox.Text;
+
+         // aPerson.FirstName = firstNameTextBox.Text;
+         // aPerson.MiddleName = middleNameTextBox.Text;
+         // aPerson.LastName = lastNameTextBox.Text;
+
+         aPerson= new Person(firstNameTextBox.Text,middleNameTextBox.Text,lastNameTextBox.Text);
+
+            firstNameTextBox.Clear();
+            middleNameTextBox.Clear();
+            lastNameTextBox.Clear();
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+           // firstNameTextBox.Text = aPerson.FirstName;
+          //  middleNameTextBox.Text = aPerson.MiddleName;
+          //  lastNameTextBox.Text = aPerson.LastName;
+
+
+           // string fullName = aPerson.GetFullName();
+            fullNameTextBox.Text = aPerson.FirstName+ ""+aPerson.MiddleName+""+aPerson.LastName;
+        }
+
+
+    }
+}
